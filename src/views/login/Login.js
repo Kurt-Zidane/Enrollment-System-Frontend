9b2c4732d15b6ac2f21b475ebdf9054a09927619
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-02-27 00:31:51 +0800

    LoginChanges

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -3,8 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 const Login = () => {
     const navigate = useNavigate();
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleRememberMeChange = (event) => {
+        setRememberMe(event.target.checked);
+      };
   
     const login = () => {
         navigate("/dashboard");
@@ -23,6 +30,15 @@ const Login = () => {
                 <div className="password-field-container">    
                     <div>Password:<FontAwesomeIcon icon={faLock} className="password-icon"/></div>
                     <input placeholder="Password" type="password" className="login-input"></input>
+                    <div className="remember-me-container">
+                <input
+                 type="checkbox"
+                 id="remember-me"
+                 checked={rememberMe}
+                 onChange={handleRememberMeChange}
+                 />
+                <label htmlFor="remember-me">Remember me</label>
+                </div>
                 </div>
                 <button className="login-btn" onClick={() => login()}>Log In</button>   
             </div>
