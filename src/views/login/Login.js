a8804339cf54b3c593148e702d5928da42f476d0
AngelV3rgs <angelacepanchito@gmail.com>
2023-02-26 23:09:21 +0800

    initial login page uodated

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,6 +1,8 @@
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 const Login = () => {
     const navigate = useNavigate();
   
@@ -9,20 +11,22 @@ const Login = () => {
     }
     return (
         <div className="main-container">
-            <div className="logo-container">
+            <div className="logo-container"> TECHNO UNIVERSITY
             </div>
             <div className="login-container">
-                <div className="login-label">LOGIN</div>
-                <div>
-                    <div>Email:</div>
+                <div className="login-label">LOG IN</div>
+                <div className="email-field-container">
+                    <div>Email: <FontAwesomeIcon icon={faEnvelope} className="email-icon"/></div>
                     <input placeholder="Sample@gmail.com" type="text" className="login-input"></input>
                 </div>
                 <div className="margin-top">
-                    <div>Password:</div>
-                    <input type="password" className="login-input"></input>
+                <div className="password-field-container">    
+                    <div>Password:<FontAwesomeIcon icon={faLock} className="password-icon"/></div>
+                    <input placeholder="Password" type="password" className="login-input"></input>
                 </div>
-                <button className="login-btn" onClick={() => login()}>Login</button>   
+                <button className="login-btn" onClick={() => login()}>Log In</button>   
             </div>
+        </div>
         </div>
     )
 
