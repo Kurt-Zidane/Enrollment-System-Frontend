05c640cf20292b41a8af83bd641d220a758e42d3
keannu125 <noehbernasol0@gmail.com>
2023-03-27 22:45:06 +0800

    Added relation between professor and subjects

--- enrollment/professor/serializers.py
+++ enrollment/professor/serializers.py
@@ -5,9 +5,12 @@ from .models import Professor
 class ProfessorSerializer(serializers.HyperlinkedModelSerializer):
     date_joined = serializers.DateTimeField(
         format="%d-%m-%Y %I:%M%p", read_only=True)
+    
+    subjects = serializers.PrimaryKeyRelatedField(
+        many=False, allow_null=True, read_only=True)
 
     class Meta:
         model = Professor
         fields = ('id', 'first_name',
-                   'last_name','age','gender','date_joined')
-        read_only_fields = ('id', 'date_joined')
+                   'last_name','age','gender','date_joined','subjects')
+        read_only_fields = ('id', 'date_joined','subjects')
